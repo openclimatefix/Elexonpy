8da3b1df1c7eecd85144b84874562a24ddf447e4
14Richa <onlinericha19@gmail.com>
2024-07-02 18:37:29 +0530

    minor fix in import

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,8 +3,6 @@ import sys
 import pytest
 import pandas as pd
 from unittest import mock
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pyelexon.datasets import Datasets
 import requests
 
