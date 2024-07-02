7e95bd26809dd49f3a1588e8254e440e5655c535
14Richa <onlinericha19@gmail.com>
2024-07-02 15:19:06 +0530

    changes tests folder

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,6 +1,10 @@
+import os
+import sys
 import pytest
 import pandas as pd
 from unittest.mock import patch, MagicMock
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pyelexon.datasets import Datasets
 import requests
 
