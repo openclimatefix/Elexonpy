21d59f96b22b7daa8a9a44f0767cd63cce4ddc8e
peterdudfield <peter.dudfield@hotmail.com>
2024-07-08 15:29:01 +0100

    add bump version and publish

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "swagger-client"
-VERSION = "1.0.0"
+version = "0.0.1"
 # To install the library, run the following
 #
 # python setup.py install
