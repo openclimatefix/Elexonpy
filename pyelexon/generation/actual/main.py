a48e3173ca9cd42e68065145842b232bbc1de924
14Richa <onlinericha19@gmail.com>
2024-07-08 20:11:39 +0530

    remove pyelexon folder

--- pyelexon/generation/actual/main.py
+++
@@ -1,8 +0,0 @@
-""" Functions for /generation/actual """
-
-""" 
- See https://developer.data.elexon.co.uk/api-details#api=prod-insol-insights-api for more information 
-This file should contain the following routes:
-"""
-
-# Path: /generation/actual/per-type
--- pyelexon/health.py
+++
@@ -1,8 +0,0 @@
-""" Functions for /health """
-
-""" 
- See https://developer.data.elexon.co.uk/api-details#api=prod-insol-insights-api for more information 
-This file should contain the following routes:
-"""
-
-# Path: /health
