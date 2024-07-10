e2f507e773eceb60f8e87d856dca48d0b5b8f09b
14Richa <onlinericha19@gmail.com>
2024-07-11 00:03:09 +0530

    remove try code

--- examples/wind_solar_forecast.py
+++ examples/wind_solar_forecast.py
@@ -14,25 +14,21 @@ forecast_api = GenerationForecastApi(api_client)
 from_date = datetime(2024, 7, 1)
 to_date = datetime(2024, 7, 7)  # Note: Maximum data output range is 7 days
 
-try:
-    # Fetch day-ahead forecast data for wind and solar from API
-    response = forecast_api.forecast_generation_wind_and_solar_day_ahead_get(
-        _from=from_date,
-        to=to_date,
-        process_type='day ahead',
-        format='json'
-    )
+# Fetch day-ahead forecast data for wind and solar from API
+response = forecast_api.forecast_generation_wind_and_solar_day_ahead_get(
+    _from=from_date,
+    to=to_date,
+    process_type='day ahead',
+    format='json'
+)
 
-    # Handle response data as needed
-    if hasattr(response, 'data'):
-        # Convert response data to DataFrame
-        df = pd.DataFrame([data.to_dict() for data in response.data])
+# Handle response data as needed
+if hasattr(response, 'data'):
+    # Convert response data to DataFrame
+    df = pd.DataFrame([data.to_dict() for data in response.data])
 
-        # Print DataFrame
-        print("\n--- Day-Ahead Wind and Solar Forecast Data ---")
-        print(df.head())
-    else:
-        print("Response object does not contain 'data' attribute.")
-
-except Exception as e:
-    print(f"Error fetching day-ahead wind and solar forecast data: {str(e)}")
+    # Print DataFrame
+    print("\n--- Day-Ahead Wind and Solar Forecast Data ---")
+    print(df.head())
+else:
+    print("Response object does not contain 'data' attribute.")
